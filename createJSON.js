99bda787931f658cd67672aa6471c4f0cebc771a
Matthew Crist <matthew.crist@boston.gov>
2016-03-15 13:16:03 -0400

    Update with a host

--- createJSON.js
+++ createJSON.js
@@ -3,6 +3,7 @@ var jsonfile = require('jsonfile')
 var file = './config/production.json'
 var obj = {
 	"database": process.env.MONGOLAB_URI,
+  "host": "0.0.0.0",
 	"port": process.env.$PORT,
 	"cron": "0 30 0 * * *"
 }
