088db6f2e3af3aae7de492808ca84467da0b4dff
Matthew Crist <matthew.crist@boston.gov>
2016-03-15 13:29:15 -0400

    Hardcoded

--- createJSON.js
+++ createJSON.js
@@ -2,7 +2,7 @@ var jsonfile = require('jsonfile')
 
 var file = './config/production.json'
 var obj = {
-	"database": process.env.MONGOLAB_URI,
+	"database": "mongodb://heroku_24phvj3q:7ssslcprnll7klpde1r83p7fk8@ds015939.mlab.com:15939/heroku_24phvj3q",
   "host": "0.0.0.0",
 	"port": process.env.$PORT,
 	"cron": "0 30 0 * * *"
