1f620d30ef73378794666b8f3450e34e0e6ad3cd
Matthew Crist <matthew.crist@boston.gov>
2016-03-23 12:18:24 -0400

    Update phantom to allow any SSL protocol

--- model/task.js
+++ model/task.js
@@ -1,15 +1,15 @@
 // This file is part of pa11y-webservice.
-// 
+//
 // pa11y-webservice is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // pa11y-webservice is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with pa11y-webservice.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -143,7 +143,9 @@ module.exports = function (app, callback) {
 						standard: task.standard,
 						timeout: (task.timeout || 30000),
 						ignore: task.ignore,
-						phantom: {},
+						phantom: {
+							"SslProtocol": "any"
+						},
 						log: {
 							debug: pa11yLog,
 							error: pa11yLog,
